227a1195e8cdd1dade6b21179ba648976c1c4320
Prabhat Singh <prbt1996@gmail.com>
2018-04-03 19:54:58 +0530

    Raj

--- app/src/main/java/com/example/prabhat/raj/AppActivity/RunTimePermissionActivity.java
+++ app/src/main/java/com/example/prabhat/raj/AppActivity/RunTimePermissionActivity.java
@@ -68,24 +68,7 @@ public class RunTimePermissionActivity extends AppCompatActivity {
         });
     }
     private void writeCalendarEvent() {
-       /* final ContentValues event = new ContentValues();
-        event.put(CalendarContract.Events.CALENDAR_ID, 1);
-        event.put(CalendarContract.Events.TITLE, "title");
-        event.put(CalendarContract.Events.DESCRIPTION, "description");
-        event.put(CalendarContract.Events.EVENT_LOCATION, "location");
-        event.put(CalendarContract.Events.DTSTART, 18000000);//startTimeMillis
-        event.put(CalendarContract.Events.DTEND, 1800000000);//endTimeMillis
-        event.put(CalendarContract.Events.ALL_DAY, 0);   // 0 for false, 1 for true
-        event.put(CalendarContract.Events.HAS_ALARM, 1); // 0 for false, 1 for true
-        String timeZone = TimeZone.getDefault().getID();
-        event.put(CalendarContract.Events.EVENT_TIMEZONE, timeZone);
-        Uri baseUri;
-        if (Build.VERSION.SDK_INT >= 8) {
-            baseUri = Uri.parse("content://com.android.calendar/events");
-        } else {
-            baseUri = Uri.parse("content://calendar/events");
-        }*/
-        //getApplicationContext().getContentResolver().insert(baseUri, event);
+
         Toast.makeText(getApplicationContext(), "Your Welcome ", Toast.LENGTH_SHORT).show();
         startActivity (new Intent (this, MapActivity.class));
     }
@@ -171,8 +154,6 @@ public class RunTimePermissionActivity extends AppCompatActivity {
     }
 
 
-
-
                 private void showDialogOK(String message, DialogInterface.OnClickListener okListener) {
                 new AlertDialog.Builder(this)
                         .setMessage(message)
@@ -181,134 +162,6 @@ public class RunTimePermissionActivity extends AppCompatActivity {
                         .create()
                         .show();
             }
-
-
-
-        /*private void insertDummyContactWrapper(){
-            @SuppressLint({"NewApi", "LocalSuppress"}) int hasWriteContactsPermission = checkSelfPermission(Manifest.permission.WRITE_CONTACTS);
-            if (hasWriteContactsPermission != PackageManager.PERMISSION_GRANTED) {
-                if (!shouldShowRequestPermissionRationale(Manifest.permission.WRITE_CONTACTS)) {
-                    showMessageOKCancel("You need to allow access to Contacts",
-                            new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    requestPermissions(new String[] {Manifest.permission.WRITE_CONTACTS},
-                                            MY_PERMISSIONS_REQUEST_WRITE_CALENDAR);
-                                }
-                            });
-                    return;
-                }
-                requestPermissions(new String[] {Manifest.permission.WRITE_CONTACTS},
-                        MY_PERMISSIONS_REQUEST_WRITE_CALENDAR);
-                return;
-            }
-            insertDummyContact();
-        }*/
-
-
-
-/*
-        Button check_permission = (Button) findViewById(R.id.check_permission);
-        Button request_permission = (Button) findViewById(R.id.request_permission);
-        check_permission.setOnClickListener(this);
-        request_permission.setOnClickListener(this);
-
-
-    }
-
-    @Override
-    public void onClick(View v) {
-
-
-        view = v;
-
-        int id = v.getId();
-        switch (id) {
-            case R.id.check_permission:
-                if (checkPermission()) {
-
-                    Snackbar.make(view, "Permission already granted.", Snackbar.LENGTH_LONG).show();
-
-                } else {
-
-                    Snackbar.make(view, "Please request permission.", Snackbar.LENGTH_LONG).show();
-                }
-                break;
-            case R.id.request_permission:
-                if (!checkPermission()) {
-
-                    requestPermission();
-
-                } else {
-
-                    Snackbar.make(view, "Permission already granted.", Snackbar.LENGTH_LONG).show();
-
-                }
-                break;
-        }
-
-    }
-
-    private boolean checkPermission() {
-        int result = ContextCompat.checkSelfPermission(getApplicationContext(), ACCESS_FINE_LOCATION);
-        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), CAMERA);
-
-        return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
-    }
-
-    private void requestPermission() {
-
-        ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION, CAMERA}, PERMISSION_REQUEST_CODE);
-
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSION_REQUEST_CODE:
-                if (grantResults.length > 0) {
-
-                    boolean locationAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-                    boolean cameraAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
-
-                    if (locationAccepted && cameraAccepted)
-                        Snackbar.make(view, "Permission Granted, Now you can access location data and camera.", Snackbar.LENGTH_LONG).show();
-                    else {
-
-                        Snackbar.make(view, "Permission Denied, You cannot access location data and camera.", Snackbar.LENGTH_LONG).show();
-
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                            if (shouldShowRequestPermissionRationale(ACCESS_FINE_LOCATION)) {
-                                showMessageOKCancel("You need to allow access to both the permissions",
-                                        new DialogInterface.OnClickListener() {
-                                            @Override
-                                            public void onClick(DialogInterface dialog, int which) {
-                                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                                                    requestPermissions(new String[]{ACCESS_FINE_LOCATION, CAMERA},
-                                                            PERMISSION_REQUEST_CODE);
-                                                }
-                                            }
-                                        });
-                                return;
-                            }
-                        }
-
-                    }
-                }
-
-
-                break;
-        }
-    }
-
-
-    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
-        new AlertDialog.Builder(RunTimePermissionActivity.this)
-                .setMessage(message)
-                .setPositiveButton("OK", okListener)
-                .setNegativeButton("Cancel", null)
-                .create()
-                .show();*/
     }
 
 
